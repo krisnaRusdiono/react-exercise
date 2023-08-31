91fbb8afd5c063af18ac5ff5a7fd8669c7686bb7
Krisna Rusdiono <krisnarusdiono@gmail.com>
2023-08-31 21:44:28 +0700

    answer: 1

--- src/routes/Test1/index.js
+++ src/routes/Test1/index.js
@@ -1,13 +1,16 @@
+import { useState } from 'react';
+
 const Test1 = () => {
+  const [value, setValue] = useState('');
   return(
     <div>
       <ul>
         <li>Please render the inputted value</li>
       </ul>
       <p>
-        <b>User Input: </b> ______
+        <b>User Input: </b> <u>{value || '______'}</u>
       </p>
-      <input type="text" placeholder="input here"/>
+      <input type="text" placeholder="input here" onChange={(e) => setValue(e.target.value)}/>
     </div>
   )
 }
